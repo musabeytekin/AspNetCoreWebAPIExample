19cd151b933834beb6d74d7fafe3245a82853910
musabeytekin <02190201003@ogr.inonu.edu.tr>
2023-08-19 19:33:45 +0300

    NLayer.Repository: Seeds and some fixes

---
+++ NLayer.Repository/Seeds/ProductFeatureSeed.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+
+namespace NLayer.Repository.Seeds
+{
+    public class ProductFeatureSeed : IEntityTypeConfiguration<ProductFeature>
+    {
+        public ProductFeatureSeed()
+        {
+        }
+
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasData(
+                new ProductFeature() { Id = 1, Color = "Red", Height = 100, Width = 200, ProductId = 1 },
+                new ProductFeature() { Id = 2, Color = "Blue", Height = 100, Width = 200, ProductId = 2 }
+            );
+        }
+    }
+}
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+
+namespace NLayer.Repository.Seeds
+{
+    public class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public ProductSeed()
+        {
+        }
+
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product() { Id = 1, CategoryId = 1, Name = "Pen 1", Price = 100, Stock = 20, CreatedDate = DateTime.Now },
+                new Product() { Id = 2, CategoryId = 1, Name = "Pen 2", Price = 100, Stock = 20, CreatedDate = DateTime.Now },
+                new Product() { Id = 3, CategoryId = 1, Name = "Pen 3", Price = 100, Stock = 20, CreatedDate = DateTime.Now },
+                new Product() { Id = 4, CategoryId = 2, Name = "Book 1", Price = 100, Stock = 20, CreatedDate = DateTime.Now },
+                new Product() { Id = 5, CategoryId = 2, Name = "Book 2", Price = 100, Stock = 20, CreatedDate = DateTime.Now }
+            );
+        }
+    }
+}
+
