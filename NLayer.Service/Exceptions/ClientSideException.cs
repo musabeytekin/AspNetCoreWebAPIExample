4b9d0d6e968763df258f471df4026ca2bad8960e
musabeytekin <02190201003@ogr.inonu.edu.tr>
2023-08-21 17:49:05 +0300

    Global Exception Handling Mechanism

---
+++ NLayer.Service/Exceptions/ClientSideException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace NLayer.Service.Exceptions
+{
+	public class ClientSideException : Exception
+	{
+		public ClientSideException(string message) : base(message)
+		{
+		}
+	}
+}
+
