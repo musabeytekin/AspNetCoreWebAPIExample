6bd38e7fcc05886a0685ea422df86b4df8566fc9
musabeytekin <02190201003@ogr.inonu.edu.tr>
2023-08-23 10:16:52 +0300

    Caching

---
+++ NLayer.API/Modules/RepoServiceModule.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+using NLayer.Repository;
+using NLayer.Service.Mapping;
+using Autofac;
+using NLayer.Repository.Repositories;
+using NLayer.Core.Repositories;
+using NLayer.Service.Services;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Repository.UnitOfWorks;
+using NLayer.Caching;
+
+namespace NLayer.API.Modules
+{
+	public class RepoServiceModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var apiAssembly = Assembly.GetExecutingAssembly();
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            builder
+                .RegisterGeneric(typeof(GenericRepository<>))
+                .As(typeof(IGenericRepository<>))
+                .InstancePerLifetimeScope();
+
+            builder
+                .RegisterGeneric(typeof(Service<>))
+                .As(typeof(IService<>))
+                .InstancePerLifetimeScope();
+
+            builder
+                .RegisterType<UnitOfWork>()
+                .As<IUnitOfWork>();
+
+            builder
+                .RegisterType<ProductServiceWithCaching>()
+                .As<IProductService>();
+
+            builder
+                .RegisterAssemblyTypes(apiAssembly, repoAssembly!, serviceAssembly!)
+                .Where(x => x.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope(); // same as AddScoped 
+
+            builder
+                .RegisterAssemblyTypes(apiAssembly, repoAssembly!, serviceAssembly!)
+                .Except<ProductService>()
+                .Where(x => x.Name.EndsWith("Service"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope(); // same as AddScoped
+        }
+    }
+}
---
+++ NLayer.Caching/ProductServiceWithCaching.cs
@@ -0,0 +1,106 @@
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using NLayer.Core;
+using NLayer.Core.DTOs;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Service.Exceptions;
+
+namespace NLayer.Caching
+{
+    public class ProductServiceWithCaching : IProductService
+    {
+        private const string CacheProductKey = "productsCache";
+        private readonly IMapper _mapper;
+        private readonly IMemoryCache _memoryCache;
+        private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductServiceWithCaching(IMapper mapper, IMemoryCache memoryCache, IProductRepository productRepository, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _memoryCache = memoryCache;
+            _productRepository = productRepository;
+            _unitOfWork = unitOfWork;
+
+            if (!_memoryCache.TryGetValue(CacheProductKey, out _))
+                _memoryCache.Set(CacheProductKey, _productRepository.GetProductsWithCategoryAsync().Result);
+        }
+
+        public async Task<Product> AddAsync(Product entity)
+        {
+            await _productRepository.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            await CacheAllProductsAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<Product>> AddRangeAsync(IEnumerable<Product> entities)
+        {
+            await _productRepository.AddRangeAsync(entities);
+            await _unitOfWork.CommitAsync();
+            await CacheAllProductsAsync();
+            return entities;
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<Product, bool>> expression)
+        {
+            return await _productRepository.AnyAsync(expression);
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return Task.FromResult(_memoryCache.Get<IEnumerable<Product>>(CacheProductKey))!;
+        }
+
+        public Task<Product> GetByIdAsync(int id)
+        {
+            var product =  _memoryCache.Get<List<Product>>(CacheProductKey)?.FirstOrDefault(x => x.Id == id) ?? throw new NotFoundException($"{nameof(Product)} ({id}) not found");
+
+            return Task.FromResult(product);
+        }
+
+        public Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategoryAsync()
+        {
+            var result = _memoryCache.Get<IEnumerable<Product>>(CacheProductKey);
+
+            var resultDto = _mapper.Map<List<ProductWithCategoryDto>>(result);
+
+            return Task.FromResult(CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, resultDto));
+        }
+
+        public async Task RemoveAsync(Product entity)
+        {
+            _productRepository.Remove(entity);
+            await _unitOfWork.CommitAsync();
+            await CacheAllProductsAsync();
+        }
+
+        public async Task RemoveRangeAsync(IEnumerable<Product> entities)
+        {
+            _productRepository.RemoveRange(entities);
+            await _unitOfWork.CommitAsync();
+            await CacheAllProductsAsync();
+        }
+
+        public async Task UpdateAsync(Product entity)
+        {
+            _productRepository.Update(entity);
+            await _unitOfWork.CommitAsync();
+            await CacheAllProductsAsync();
+        }
+
+        public IQueryable<Product> Where(Expression<Func<Product, bool>> expression)
+        {
+            return _memoryCache.Get<List<Product>>(CacheProductKey)!.Where(expression.Compile()).AsQueryable();
+        }
+
+        public async Task CacheAllProductsAsync()
+        {
+            _memoryCache.Set(CacheProductKey, await _productRepository.GetAll().ToListAsync());
+        }
+    }
+}
